e3b37f0ba8c8b0c0ea8b981baa7f55698c573b0e
chryshumble <ibetochrys20@gmail.com>
2023-12-11 20:34:38 +0100

    my first game

---
+++ alien_invasion.py
@@ -0,0 +1,22 @@
+import sys
+import pygame
+class AlienInvasion:
+    """overall class to manage game assets and behaviour"""
+    def _init_(self):
+        """initialise the game, and create game resources"""
+        pygame.init()
+        self.screen = pygame.display.set_mode((1200, 800))
+        pygame.display.set_caption("Alien Invasion")
+    def run_game(self):
+        """start the main loop for the game."""
+        while True:
+            #watch for keyboard and mouse events.
+            for event in pygame.event.get():
+                if event.type ==pygame.QUIT:
+                    sys.exit()
+#make the most recently drawn screen visible.
+pygame.display.flip()
+if __name__ == '__main__':
+    #make a game instance, and run the game.
+    ai = AlienInvasion()
+    ai.run_game
